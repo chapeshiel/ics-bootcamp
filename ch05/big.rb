a8ffca3c60cf6786805d2162cd09e10b1453d196
Chape <chapeshiel31@gmail.com>
2017-12-20 14:58:41 -0800

    ch05

---
+++ ch05/big.rb
@@ -0,0 +1,3 @@
+puts 'What is your favorite number'
+favorite_number = gets.chomp
+puts (favorite_number.to_i + 1).to_s + 'is a better and bigger number in my opinion'
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,4 @@
+puts 'Hello what is your first, middle, and last name'
+first = gets.chomp + middle = gets.chomp + last_name = gets.chomp
+puts 'Your name is ' + first + middle + last_name +'? What an awesome name!'
+puts 'Pleased to meet you ' + first + middle + last_name +'. :D'
