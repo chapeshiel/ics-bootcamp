94d7e11fd4f3885f4915222efc430e3919f372a2
Chape <chapeshiel31@gmail.com>
2017-12-28 13:00:55 -0800

    ch05

--- ch05/fullNameGreeting.rb
+++ ch05/fullNameGreeting.rb
@@ -1,4 +1,8 @@
-puts 'Hello what is your first, middle, and last name'
-first = gets.chomp + middle = gets.chomp + last_name = gets.chomp
-puts 'Your name is ' + first + middle + last_name +'? What an awesome name!'
-puts 'Pleased to meet you ' + first + middle + last_name +'. :D'
+puts 'Hello, what is your first name?'
+first_name = gets.chomp
+puts 'What is your middle name?'
+middle_name = gets.chomp
+puts 'What is your last name?'
+last_name = gets.chomp
+puts 'Your name is ' + first_name +' '+ middle_name +' '+ last_name + '? What an awesome name!'
+puts 'Pleased to meet you ' + first_name + ' ' + middle_name +' '+ last_name +'. :D'
