77c5920f330fac2fabde6d4c137b4447cf4ccf0c
Chape <chapeshiel31@gmail.com>
2017-12-29 22:46:50 -0800

    ch06

---
+++ ch06/table.rb
@@ -0,0 +1,6 @@
+line_width = 40
+puts ('Table of Contents'.center(line_width*1.1))
+puts ''
+puts ('Chapter 1:  Getting Started'.ljust(line_width) + 'page  1'.ljust(line_width/2))
+puts ('Chapter 2:  Numbers'.ljust(line_width) + 'page  9'.ljust(line_width/2))
+puts ('Chapter 3:  Letters'.ljust(line_width) + 'page 13'.ljust(line_width/2))
