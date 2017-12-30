d289d193c7de579c584518c61df04f7961b3a74c
Chape <chapeshiel31@gmail.com>
2017-12-29 22:48:03 -0800

    ch08

---
+++ ch08/sortArray.rb
@@ -0,0 +1,8 @@
+puts 'Type some things!'
+
+words = []
+puts words
+while (thing = gets.chomp) != ''
+  words.push thing
+end
+puts words.sort
---
+++ ch08/table.rb
@@ -0,0 +1,6 @@
+line_width = 40
+puts 'Table of Contents'.center(line_width)
+contents = [['Chapter 1: Getting Started', 'page 1'],['Chapter 2: Numbers', 'page 9'],['Chapter 3: Letters', 'page 13']]
+contents.each do |con|
+	puts con[0].ljust(line_width) + con[1].rjust(line_width)
+end
