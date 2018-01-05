063b585d02a5fb358ed5de073d4048e836b40e33
Chape <chapeshiel31@gmail.com>
2018-01-05 15:11:17 -0800

    ch06

--- ch06/angryBoss.rb
+++ ch06/angryBoss.rb
@@ -1,3 +1,3 @@
 puts 'What do you want?!'
 answer = gets.chomp
-puts 'WHADDAYA MEAN' +' '+ answer.upcase + '?!? YOU ARE FIRED!!'
+puts 'WHADDAYA MEAN' +' "'+ answer.upcase + '"' '?!? YOU ARE FIRED!!'
